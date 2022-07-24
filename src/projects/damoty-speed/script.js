fa806e9534e408431621e2d1e23ebb6395190820
dzakifadh <fadh.dzaki@gmail.com>
2022-07-24 22:04:03 +0700

    build: :art: Add 4 section for damoty-speed project
    On progress slicing section, will be continue next days
    
    1. Hero 2.Brands

--- src/projects/damoty-speed/script.js
+++ src/projects/damoty-speed/script.js
@@ -1,1 +1,36 @@
-console.log("Sub");
+import { tns } from "tiny-slider/src/tiny-slider";
+import "/node_modules/tiny-slider/src/tiny-slider.scss";
+
+var slider = tns({
+	container: "#logo-brands",
+	mouseDrag: true,
+	swipeAngle: false,
+	nav: false,
+	autoplayButtonOutput: false,
+	controls: false,
+	autoplay: true,
+	speed: 500,
+	responsive: {
+		0: {
+			items: 1.1,
+		},
+		320: {
+			items: 1.2,
+		},
+		360: {
+			items: 1.3,
+		},
+		380: {
+			items: 1.4,
+		},
+		768: {
+			items: 2,
+		},
+		992: {
+			items: 3,
+		},
+		1200: {
+			items: 4,
+		},
+	},
+});
